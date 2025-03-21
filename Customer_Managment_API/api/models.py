e9b2c2f24967525194ba6ca4671c60b0825aa0eb
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-03-21 20:29:58 +0100

    Created CustomUserManager & added Is authenticated to views

--- Customer_Managment_API/api/models.py
+++ Customer_Managment_API/api/models.py
@@ -1,7 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models
+
+class CustomUserManager(BaseUserManager):
+    # This method is used to create and save a regular user.
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    #This method is used to create and save a superuser (an admin-level user with all permissions).
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
 
 class CustomUser(AbstractUser):
     username = None # Removed the username field
@@ -10,6 +35,10 @@ class CustomUser(AbstractUser):
     # Setting email as the unique identifier for authentication
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    #The CustomUserManager is assigned to the custom user model through the objects attribute
+    objects = CustomUserManager()
+
 
     def __str__(self):
         return self.email
@@ -37,4 +66,7 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} for {self.customer} made at {self.order_date}"
+
+
+
 
--- Customer_Managment_API/api/views.py
+++ Customer_Managment_API/api/views.py
@@ -4,20 +4,35 @@ from .models import Customer, Order
 from .serializers import CustomerSerializer, OrderSerializer
 
 
+from rest_framework.permissions import IsAuthenticated
+
+
 # Create your views here.
 
 class CustomerListCreateAPIView(generics.ListCreateAPIView):
+    
+    permission_classes = [IsAuthenticated]
+
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
 class CustomerRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    
+    permission_classes = [IsAuthenticated]
+    
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
 class OrderListCreateAPIView(generics.ListCreateAPIView):
+    
+    permission_classes = [IsAuthenticated]
+
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
 class OrderRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    
+    permission_classes = [IsAuthenticated]
+
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
