0394360c5914323f532a6ba03cc707998381f001
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-03-21 19:50:45 +0100

    Created views and serializers for Customer & Order

--- Customer_Managment_API/api/urls.py
+++ Customer_Managment_API/api/urls.py
@@ -1,3 +1,12 @@
-from django.urls import path, include
+from django.urls import path
+from .views import (CustomerListCreateAPIView,
+                    CustomerRetrieveUpdateDestroyAPIView,
+                    OrderListCreateAPIView,
+                    OrderRetrieveUpdateDestroyAPIView)
 
-urlpatterns = []
+urlpatterns = [
+    path('customers', CustomerListCreateAPIView.as_view(), name='customers'),
+    path('customer/<int:pk>', CustomerRetrieveUpdateDestroyAPIView.as_view(), name='customer'),
+    path('orders', OrderListCreateAPIView.as_view(), name='orders'),
+    path('orders/<int:pk>', OrderRetrieveUpdateDestroyAPIView.as_view(), name='order'),
+]
--- Customer_Managment_API/api/views.py
+++ Customer_Managment_API/api/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Customer, Order
+from .serializers import CustomerSerializer, OrderSerializer
 
 
 # Create your views here.
+
+class CustomerListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CustomerRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class OrderListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
