b00f4348eab3712aecd2cf6a875b9f62023ace8c
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-03-29 21:19:13 +0100

    Implemented Search, Filtering and Ordering Features

--- Customer_Managment_API/Customer_Managment_API/settings.py
+++ Customer_Managment_API/Customer_Managment_API/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'rest_framework_simplejwt',
-    
+    'django_filters',    
 ]
 
 MIDDLEWARE = [
--- Customer_Managment_API/api/views.py
+++ Customer_Managment_API/api/views.py
@@ -3,6 +3,10 @@ from rest_framework import viewsets
 from .models import Customer, Order
 from .serializers import CustomerSerializer, OrderSerializer
 
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+
 
 from rest_framework.permissions import IsAuthenticated
 
@@ -10,13 +14,30 @@ from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class CustomerModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    #permission_classes = [IsAuthenticated]
 
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    
+    filterset_fields = ['name', 'email']
+    
+    search_fields = ['name', 'email','address','phone_number']
+    
+    ordering_fields = ['name', 'created_at']
+
+
 class OrderModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    #permission_classes = [IsAuthenticated]
 
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    
+    filterset_fields = ['customer', 'order_date', 'status', 'total']
+    
+    search_fields = ['customer']
+    
+    ordering_fields = ['customer', 'status', 'total']
