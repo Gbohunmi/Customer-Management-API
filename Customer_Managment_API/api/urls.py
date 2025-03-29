1c5eaf505a0a79ddd694242b0522709ba0949e45
Gbohunmi <gbohunmi.odutuga@gmail.com>
2025-03-29 20:58:54 +0100

    Changed url pattern for api

--- Customer_Managment_API/api/urls.py
+++ Customer_Managment_API/api/urls.py
@@ -16,8 +16,7 @@ router.register(r'orders', OrderModelViewSet, basename='order')
 
 
 urlpatterns = [
-    path('api/', include(router.urls)),
-
+    path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
